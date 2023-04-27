2a5ce0d2a0f8ad1c927e1fa54e811c7d365bf740
Frangrota <105676118+Frangrota@users.noreply.github.com>
2023-04-27 10:26:16 +0200

    Update index.php

--- login/index.php
+++ login/index.php
@@ -1,29 +1,3 @@
-<?php 
-
-if (isset($_POST['uname']) && isset($_POST['password'])) {
-    function validate($data){
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
-    $uname = validate($_POST['uname']);
-    $pass = validate($_POST['password']);
-
-    if (empty($uname)) {
-            header("Location: login.php?error=User Name is required");
-    }else if(empty($pass)){
-        header("Location: login.php?error=Password is required");
-    }else{
-        echo "Valid input";
-    }
-       
-} else{
-    header("Location: login.php?error");
-    exit ();
-}
-?>
 <head>
 	<title>Czat</title>
     <link rel="stylesheet" href="style3.css">
